db5859c73d63a32f5ee30ff69736bf3517646100
Conner Petzold <cpetzold@gmail.com>
2012-12-10 22:35:13 -0500

    autocomplete w/ wordnik

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject jnput "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [webfui "0.2.1"]]
+                 [webfui "0.2.1"]
+                 [jayq "0.3.2"]]
   :plugins [[lein-cljsbuild "0.2.9"]]
   :cljsbuild { :builds [
    {:source-path "src"
--- src/cljs/jnput/core.cljs
+++ src/cljs/jnput/core.cljs
@@ -1,27 +1,69 @@
 (ns jnput.core
   (:use-macros [webfui.framework.macros :only [add-dom-watch]])
   (:require [webfui.framework :as fui]
-            [clojure.browser.dom :as dom]))
+            [clojure.browser.dom :as dom]
+            [clojure.walk :as walk]
+            [jayq.core :as $]))
+
+(def wordnik-base "http://api.wordnik.com//v4/words.json/search/")
+
+(defn res-map [res]
+  (walk/keywordize-keys (js->clj res)))
+
+(defn get-word [q cb]
+  ($/ajax (str wordnik-base q ".*?allowRegex=true&api_key=997ea3a64b190c6d8f0040df7b6003393e51bbd224bc5ec8d")
+          {:dataType "jsonp"
+           :success (fn [res]
+                      (cb
+                       (when-let [word (get-in (res-map res) [:searchResults 1 :word])]
+                         word)))}))
 
 (def tweet-length 140)
 
-(def initial-state {:input "hello world"})
+(def initial-state
+  {:tweet ""
+   :search {:query "" :result ""}})
 
 (def state (atom initial-state))
 
-(add-dom-watch :in-watch [state new]
-               (dom/log state)
+(add-dom-watch :tweet-watch [s new]
                (let [{:keys [value]} (second new)]
                  (when (< (count value) (inc tweet-length))
-                   {:input value})))
+                   {:tweet value})))
 
-(defn render [state]
-  (let [{:keys [input]} state]
+(add-dom-watch :search-watch [s new]
+               (let [{:keys [value]} (second new)
+                     patch {:search {:query value}}
+                     query (-> s :search :query)
+                     result (-> s :search :result)]
+                 (if (and result
+                          (= 0 (.indexOf result value))
+                          (< query value))
+                   patch
+                   (do
+                     (when-not (empty? value)
+                       (get-word value
+                                 (fn [word]
+                                   (when (= value (-> @state :search :query))
+                                     (swap! state assoc-in [:search :result] word)))))
+                     (assoc-in patch [:search :result] value)))))
+                     
+(defn render [s]
+  (let [{:keys [tweet search]} s]
     [:div
-     [:input#in {:watch :in-watch
-                 :value input
-                 :autofocus true}]
-     [:input#auto {:value (str input (- tweet-length (count input)))
-                   :disabled true}]]))
+     [:div#tweet
+      [:input#tweet-in {:watch :tweet-watch
+                        :value tweet
+                        :placeholder "Tweet..."}]
+      [:input#tweet-count {:value (if (empty? tweet) ""
+                                      (str tweet (- tweet-length (count tweet))))
+                           :disabled true}]]
+     [:div#search
+      [:input#search-query {:watch :search-watch
+                            :value (:query search)
+                            :autofocus true
+                            :placeholder "Search..."}]
+      [:input#search-res {:value (:result search)
+                          :disabled true}]]]))
 
 (fui/launch-app state render)
